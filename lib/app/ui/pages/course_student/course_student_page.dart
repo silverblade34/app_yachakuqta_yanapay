ad7333d83919fe58f137416cdf230fc9a3f08c97
Michell Pacheco Tacay <mpacheco.tacay@gmail.com>
2024-05-06 10:32:56 -0500

    se esta mostrando imagen de course background como encabezado en course_student_page

--- lib/app/ui/pages/course_student/course_student_page.dart
+++ lib/app/ui/pages/course_student/course_student_page.dart
@@ -1,4 +1,6 @@
 import 'package:app_yachakuqta_yanapay/app/controllers/course_student_controller.dart';
+import 'package:app_yachakuqta_yanapay/app/utils/global_utils.dart';
+import 'package:app_yachakuqta_yanapay/app/utils/style_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,16 +9,63 @@ class CourseStudentPage extends GetView<CourseStudentController> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            controller.courseData.title.toUpperCase(),
-            style: const TextStyle(fontSize: 17),
-          ),
-          elevation: 4,
+      appBar: AppBar(
+        title: Text(
+          controller.courseData.title.toUpperCase(),
+          style: const TextStyle(fontSize: 17),
         ),
-        body: SafeArea(child: Column(
-          children: [Container()],
-        ),),);
+        elevation: 4,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 250,
+              width: screenWidth - 20,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 32, 32, 32),
+                image: DecorationImage(
+                  image: NetworkImage(controller.courseData.imageBackground !=
+                          ""
+                      ? '$url$versionService$methodGetImageCourse${controller.courseData.imageBackground}'
+                      : "https://img.freepik.com/vector-gratis/ilustracion-libro-lectura_114360-8532.jpg?t=st=1715006993~exp=1715010593~hmac=3176fad2eeb7e1181edf3cdebe585ad4142921f578de4ed3942c1bf7422e2fc3&w=740"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.dstATop,
+                  ),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom:
+                        10, // Ajusta la posición vertical según sea necesario
+                    child: Text(
+                      controller.courseData.title.toUpperCase(),
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/app/utils/style_utils.dart
+++ lib/app/utils/style_utils.dart
@@ -16,6 +16,7 @@ const Color PRIMARY_LIGHT = Color.fromARGB(255, 248, 171, 76);
 const Color TERTIARY = Color(0xFFA2A629);
 const Color OTHER = Color(0xFFD93DA2);
 const Color BACKGROUND_LIGHT_CREAM = Color.fromARGB(251, 240, 232, 208);
+const Color BACKGROUND_LIGHT = Color.fromARGB(255, 248, 248, 248);
 
 const Color GREY_HARD = Color.fromARGB(255, 47, 55, 71);
 const Color GREY_LIGHT = Color.fromARGB(255, 100, 100, 100);
